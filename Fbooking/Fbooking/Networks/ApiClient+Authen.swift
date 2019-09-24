c66d7a9fb7957fb5048c40981e38da548313e661
Tuan Vu <shjn@Tuans-MacBook-Pro.local>
2019-09-24 14:42:52 +0700

    login with account kit

--- Fbooking/Fbooking/Networks/ApiClient+Authen.swift
+++ Fbooking/Fbooking/Networks/ApiClient+Authen.swift
@@ -1,16 +1,25 @@
 import Alamofire
 
 extension ApiClient {
-    static func loginWith(email: String,
-                          password: String,
+    static func loginAccountKid(authoriserCode: String,
+                          phone: String?,
+                          deviceIdentify: String?,
                           success: @escaping ResponseSuccess,
                           fail: @escaping ResponseFailure) -> Request? {
         // create url
         let url = Config.baseUrl + "/api/sandbox/user/login"
         // create params
         var params = ApiClient.defaultParams()
-        params["username"] = email
-        params["password"] = password
+        params["serviceName"] = "AUTH_LOGIN_ACCOUNTKIT"
+        params["authoriserCode"] = authoriserCode
+        params["phone"] = phone
+        params["appType"] = "IOS"
+        if let _deviceIdentify = deviceIdentify {
+            params["deviceIdentify"] = _deviceIdentify
+        } else {
+            params["deviceIdentify"] = NSNull()
+        }
+        params["lang"] = "vi"
         // create headers
         let headers = [HeaderKey.ContentType: HeaderValue.ApplicationJson]
         return sessionWithoutToken.request(url, method: .post, parameters: params,
--- Fbooking/Fbooking/ViewControllers/SignIn/SignInViewController.swift
+++ Fbooking/Fbooking/ViewControllers/SignIn/SignInViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 import AccountKit
 class SignInViewController: UIViewController {
-    var accountKit: AccountKitManager!
+    var accountKit: AccountKitManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.navigationController?.isNavigationBarHidden = true
         if accountKit == nil {
-            accountKit = AccountKitManager(responseType: .accessToken)
+            accountKit = AccountKitManager(responseType: .authorizationCode)
         }
        loginWithPhone()
     }
@@ -24,11 +24,10 @@ class SignInViewController: UIViewController {
         super.viewWillAppear(animated)
         if accountKit?.currentAccessToken != nil {
             // if the user is already logged in, go to the main screen
-            self.gotoHomeViewController()
+//            self.gotoHomeViewController()
         }
         else {
             // Show the login screen
-            
         }
     }
     
@@ -41,6 +40,7 @@ class SignInViewController: UIViewController {
         let inputState = UUID().uuidString
         let vc = (accountKit?.viewControllerForPhoneLogin(with: nil, state: inputState))!
         vc.isSendToFacebookEnabled = true
+        vc.defaultCountryCode = "VN"
         self.prepareLoginViewController(loginViewController: vc)
         self.present(vc as UIViewController, animated: true, completion: nil)
     }
@@ -55,11 +55,19 @@ extension SignInViewController: AKFViewControllerDelegate {
     // ACCOUNT KIT DELEGATE
     func viewController(_ viewController: UIViewController & AKFViewController, didCompleteLoginWith code: String, state: String) {
         print("didCompleteLoginWith code")
+        _ = ApiClient.loginAccountKid(authoriserCode: code, phone: nil, success: { (response) in
+            print(response)
+        }, fail: { (statusCode, error) in
+            print("status: \(statusCode)")
+            print("error: \(String(describing: error?.description))")
+        })
+
     }
     func viewController(_ viewController: UIViewController & AKFViewController, didCompleteLoginWith accessToken: AKFAccessToken, state: String) {
         print("didCompleteLoginWith accessToken")
-        self.gotoHomeViewController()
+        //        self.gotoHomeViewController()
     }
+
     func viewControllerDidCancel(_ viewController: UIViewController & AKFViewController) {
         
     }
